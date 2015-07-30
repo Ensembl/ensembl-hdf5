045554a2462c372ff12a9f10ee7c51500456623e
Daniel Zerbino <zerbino@ebi.ac.uk>
2015-07-30 13:32:32 +0100

    Revert "Removing useless files"
    This reverts commit 0f41d7f5589e1086fce38a2289244b3f781058b1.

---
+++ xs/toto.sh
@@ -0,0 +1,1 @@
+cd ../c ; make ; cd ../xs ; touch HDF5.xs ; make ; cp blib/arch/auto/Bio/EnsEMBL/HDF5/HDF5.so ~/.local/lib/site_perl/5.14.2/x86_64-linux-thread-multi/auto/Bio/EnsEMBL/HDF5/HDF5.so ; perl t/Bio-EnsEMBL-HDF5.t
